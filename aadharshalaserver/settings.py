bcc5111b07b02763c4472b9dbf6e1ba78ec7368a
Palash Mittal <pmittal00@gmail.com>
2021-10-31 17:53:49 +0530

    Change allowed hosts

--- aadharshalaserver/settings.py
+++ aadharshalaserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%bp21921$_i3-8l$9&46=!4od*(g8d6xrgv74foccl9@a*dk_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://10.0.2.2/']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
 
 
 # Application definition
